bdb78293d70164c1e0af0fc732e2d47614474426
Kira Ariyan <kna2121@columbia.edu>
2025-09-28 20:58:40 +0000

    fix error

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,8 +1,8 @@
 class Movie < ActiveRecord::Base
 
   def self.with_ratings(ratings_list)
-    if ratings_list.nil?
-      return Movie.all
+    if ratings_list.nil? || ratings_list.empty?
+      all
     else
       where(rating:ratings_list)
     end
